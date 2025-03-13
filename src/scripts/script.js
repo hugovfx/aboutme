0a6e53030e4ee50532fc666224b22e967eac0f9c
HugoVFX <hugogamesbaezq@gmail.com>
2025-03-12 20:50:57 -0600

    Profile pic and icons center

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,32 +1,29 @@
-document.addEventListener("mousemove", (event) => {
-    const bubbles = document.querySelectorAll(".bubble");
-
-    bubbles.forEach((bubble) => {
-        const bubbleRect = bubble.getBoundingClientRect();
-        const bubbleX = bubbleRect.left + bubbleRect.width / 2;
-        const bubbleY = bubbleRect.top + bubbleRect.height / 2;
-
-        const dx = event.clientX - bubbleX;
-        const dy = event.clientY - bubbleY;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-
-        if (distance < 100) {
-            const angle = Math.atan2(dy, dx);
-            const moveX = -Math.cos(angle) * 20; // Movemos en dirección opuesta
-            const moveY = -Math.sin(angle) * 20; // Movemos en dirección opuesta
-
-            bubble.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.2)`;
-        } else {
-            bubble.style.transform = "scale(1)";
+lucide.createIcons();
+        
+        function toggleExpand(element) {
+            const isExpanded = element.classList.contains('expanded');
+            
+            let contactItems = document.querySelectorAll('.contact-item');
+            contactItems.forEach(item => {
+                item.classList.remove('expanded');
+            });
+            
+            if (!isExpanded) {
+                element.classList.add('expanded');
+            }
         }
-    });
-});
 
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
+        window.addEventListener("scroll", function () {
+            let presentation = document.getElementById("presentation");
+            
+            if (window.scrollY > 50) {
+                presentation.classList.add("navbar");
+            } else {
+                presentation.classList.remove("navbar");
+            }
+        });
+        
+        document.querySelector('.scroll-arrow').addEventListener('click', function() {
+            const infoSection = document.querySelector('.info');
+            infoSection.scrollIntoView({ behavior: 'smooth' });
+        });
