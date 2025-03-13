f46a5f66fe53923548e463451be14a7ab5c5a9b3
HugoVFX <hugogamesbaezq@gmail.com>
2025-03-12 21:04:02 -0600

    scroll fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,29 +1,35 @@
 lucide.createIcons();
-        
-        function toggleExpand(element) {
-            const isExpanded = element.classList.contains('expanded');
-            
-            let contactItems = document.querySelectorAll('.contact-item');
-            contactItems.forEach(item => {
-                item.classList.remove('expanded');
-            });
-            
-            if (!isExpanded) {
-                element.classList.add('expanded');
-            }
-        }
 
-        window.addEventListener("scroll", function () {
-            let presentation = document.getElementById("presentation");
-            
-            if (window.scrollY > 50) {
-                presentation.classList.add("navbar");
-            } else {
-                presentation.classList.remove("navbar");
-            }
-        });
-        
-        document.querySelector('.scroll-arrow').addEventListener('click', function() {
-            const infoSection = document.querySelector('.info');
-            infoSection.scrollIntoView({ behavior: 'smooth' });
-        });
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
+
+function toggleExpand(element) {
+    const isExpanded = element.classList.contains('expanded');
+    
+    let contactItems = document.querySelectorAll('.contact-item');
+    contactItems.forEach(item => {
+        item.classList.remove('expanded');
+    });
+    
+    if (!isExpanded) {
+        element.classList.add('expanded');
+    }
+}
+
+window.addEventListener("scroll", function () {
+    let presentation = document.getElementById("presentation");
+    
+    if (window.scrollY > 10) {
+        presentation.classList.add("navbar");
+    } else {
+        presentation.classList.remove("navbar");
+    }
+});
+
+document.querySelector('.scroll-arrow').addEventListener('click', function() {
+    window.scrollTo({
+        top: 11,
+        behavior: 'smooth'
+    });
+});
